7e5e41d89b59f677f0b5e40f36d6a2da35e3d127
Amber <amberhe613@gmail.com>
2018-01-20 02:57:14 -0800

    add mongodb

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
-require("./services/passport");
 const mongoose = require("mongoose");
-const authRoutes = require("./routes/authRoutes");
 const keys = require("./config/keys");
 require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-authRoutes(app);
+require("./routes/authRoutes")(app);
 
 //Set up serves
 const PORT = process.env.PORT || 5000;
