e17db4b19c9ec3ef21cd0d2c3eb5807720255d92
Amber <amberhe613@gmail.com>
2018-01-20 02:46:33 -0800

    add mongodb

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID: "831729423679-011sidk96uq0qh3i4b0gmvb0fvnsp96i.apps.googleusercontent.com",
-    googleClientSecret: "36j2d4cwKdLIicxE6NFwv7s1"
+    googleClientSecret: "36j2d4cwKdLIicxE6NFwv7s1",
+    mongoURI: "mongodb://admin:123@ds263847.mlab.com:63847/heroku_8wzbqsqx"
 }
--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("./services/passport");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 const keys = require("./config/keys");
+require("./models/User");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-// Set google OAuth with passport.js
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: "/auth/google/callback"
-        },
-        (accessToken, refreshToken, profile, done) => {
-            console.log("access token", accessToken);
-            console.log("refresh token", refreshToken);
-            console.log("profile", profile);
-        }
-    )
-);
-
-app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+authRoutes(app);
 
 //Set up serves
 const PORT = process.env.PORT || 5000;
