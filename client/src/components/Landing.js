d5c7a617ac095b466c14ff4ecf26e15ad6be736b
Amber <amberhe613@gmail.com>
2018-01-22 20:31:53 -0800

    Done with header and basic route

---
+++ client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center'}}>
+            <h4>
+                Looking for a simple, poweful online survey tool?
+            </h4>
+            <h2 style={{ fontWeight: 'bolder', color: '#ee6e73'}}>
+                SurveyStar is the answer.
+            </h2>
+            SurveyStar offers a tremendous set of free tools for designing your survey, sharing your survey online, and reviewing your survey's results. We've got a fantastic user experience and tons of great features.
+        </div>
+    )
+}
+
+export default Landing;
