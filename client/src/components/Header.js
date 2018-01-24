19522feed2900c121e3c08e36dcfb41ca3cc8838
Amber <amberhe613@gmail.com>
2018-01-23 20:37:04 -0800

    Add Payment with stripe

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
         renderContent() {
@@ -8,11 +9,15 @@ class Header extends Component {
                 case null:
                     return;
                 case false:
-                    return <li><a href="/auth/google">Login With Google</a></li>;
+                    return <li><a href="/auth/google">LOGIN WITH GOOGLE</a></li>;
             default:
-                return (
-                    <li><a href="/api/logout">Log out</a></li>
-                );
+                return [
+                    <li key="1"><Payments/></li>,
+                    <li key="2"  style={{ margin: "0 10px" }}>
+                        CREDITS: {this.props.auth.credits}
+                    </li>,
+                    <li key="3"><a href="/api/logout">LOG OUT</a></li>
+                ]
         }
     }
 
